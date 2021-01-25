a5015a198f633b1a9685bcabd802ff6bc0d39149
UraNus0205 <787551776@qq.com>
2021-01-25 16:47:15 +0800

    1.25

---
+++ codeup/算法笔记3.6/D单词替换/D单词替换.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <cstring>
+#include <cstdio>
+#include <string>
+using namespace std;
+int main()
+{
+    string s;
+    while (getline(cin, s))
+    {
+        char a[110], b[110];
+        gets_s(a);
+        gets_s(b);
+        int la = strlen(a), lb = strlen(b);
+        char res[110][110];
+        int row = 0, col = 0;
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (s[i] != ' ') res[row][col++] = s[i];
+            else {
+                res[row++][col] = '\0';
+                col = 0;
+            }
+        }
+        res[row][col] = '\0';
+        for (int i = 0; i <= row; i++)
+        {
+            
+            if (strcmp(a, res[i]) == 0)
+            {
+                int j = 0;
+                for (j = 0; j < lb; j++)
+                {
+                    res[i][j] = b[j];
+                }
+                res[i][j] = '\0';
+            }
+            
+        }
+        for (int i = 0; i <= row; i++)
+        {
+            cout << res[i];
+            if (i != row) cout << " ";
+            else cout << endl;
+        }
+    }
+    return 0;
+}
---
+++ codeup/算法笔记3.6/H编排字符串/H编排字符串.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <cstring>
+#include <string.h>
+using namespace std;
+int main()
+{
+    char s[4][30] = { {""} ,{""}, {""}, {""} };
+    int m;
+    cin >> m;
+    getchar();
+    while (m--)
+    {
+        int k = 0;
+        char c[30];
+        
+        gets_s(c);
+        for (int i = 3; i >=0; i--)
+        {
+            if (i != 0)
+            {
+                int j = 0;
+                for (j = 0; j < strlen(s[i - 1]); j++)
+                {
+                    s[i][j] = s[i - 1][j];
+                }
+                s[i][j] = '\0';
+            }
+            else
+            {
+                int j = 0;
+                for (j = 0; j < strlen(c); j++)
+                {
+                    s[i][j] = c[j];
+                }
+                s[i][j] = '\0';
+            }
+        }
+        for (int i = 0; i < 4; i++)
+        {
+            if(strlen(s[i]))cout << i + 1 << "=" << s[i];
+            if (i != 3) cout << " ";
+            else cout << endl;
+        }
+    }
+    return 0;
+}
