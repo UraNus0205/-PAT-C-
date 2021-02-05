4cf7557621f8c456223ab1f77303f39c90132917
UraNus0205 <787551776@qq.com>
2021-02-05 22:58:59 +0800

    2.5

---
+++ codeup/算法笔记4.4/B出租车费/B出租车费.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+int main()
+{
+    int n;
+    while (cin >> n && n != 0)
+    {
+        double res = 0;
+        if (n <= 4) res = 10;
+        else if (n > 4 && n <= 8) res = 10 + (n - 4) * 2;
+        else if (n > 8)
+        {
+            while (n > 8)
+            {
+                res += 18;
+                n -= 8;
+            }
+            if (n <= 4) res += 2.4 * n;
+            if (n > 4 && n <= 8) res += 10 + 2 * (n - 4);
+        }
+        cout << res << endl;
+    }
+    return 0;
+}
---
+++ codeup/算法笔记4.4/DRepair the Wall/DRepair the Wall.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <string>
+#include <cstring>
+int blocks[610];
+
+using namespace std;
+
+bool cmp(int a, int b)
+{
+	return a > b;
+}
+
+int main()
+{
+	int l, n;
+	while (cin >> l >> n)
+	{
+		memset(blocks, 0, sizeof(blocks));
+		for (int i = 0; i < n; i++)
+		{
+			cin >> blocks[i];
+		}
+		sort(blocks, blocks + n, cmp);
+		int sum = 0, res = 0, i = 0;;
+		for (i = 0; i < n; i++)
+		{
+			if (res + blocks[i] >= l)
+			{
+				sum++;
+				break;
+			}
+			else
+			{
+				sum++;
+				res += blocks[i];
+			}
+		}
+		if (i == n)
+		{
+			cout << "impossible" << endl;
+		}
+		else
+			cout << sum << endl;
+	}
+	return 0;
+}
