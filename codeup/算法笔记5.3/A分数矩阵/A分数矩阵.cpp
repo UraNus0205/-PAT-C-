382678109fbde8c29883d2a98cb52649bcdd746e
UraNus0205 <787551776@qq.com>
2021-02-22 22:10:15 +0800

    2021.2.22

---
+++ codeup/算法笔记5.3/A分数矩阵/A分数矩阵.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main()
+{
+    double n;
+    while (cin >> n && n != 0)
+    {
+        double sum = 0;
+        sum += n;
+        double down = 2;
+        for (int i = n; i > 1; i--)
+        {
+            sum += 1.0 / i * down;
+            down = down + 2;
+        }
+        printf_s("%.2f\n", sum);
+    }
+    return 0;
+}
+/*
+1
+22
+342
+4642
+58642
+*/
---
+++ codeup/算法笔记5.3/分数的四则运算/分数的四则运算.cpp
@@ -0,0 +1,80 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <cstdio>
+using namespace std;
+struct Fraction {
+    int up, down;
+};
+
+int gcd(int a, int b)
+{
+    if (a < b) swap(a, b);
+    if (b == 0) return a;
+    else return gcd(b, a % b);
+}
+
+Fraction reduction(Fraction result)
+{
+    if (result.down < 0)
+    {
+        result.up = -result.up;
+        result.down = -result.down;
+    }
+    if (result.up == 0) result.down = 1;
+    else
+    {
+        int d = gcd(result.up, result.down);
+        result.up /= d;
+        result.down /= d;
+    }
+    return result;
+}
+
+Fraction add(Fraction f1, Fraction f2)
+{
+    Fraction result;
+    result.up = f1.up * f2.down + f1.down * f2.up;
+    result.down = f1.down * f2.down;
+    return reduction(result);
+}
+
+Fraction sub(Fraction f1, Fraction f2)
+{
+    Fraction result;
+    result.up = f1.up * f2.down - f1.down * f2.up;
+    result.down = f1.down * f2.down;
+    return reduction(result);
+}
+
+Fraction mul(Fraction f1, Fraction f2)
+{
+    Fraction result;
+    result.up = f1.up * f2.up;
+    result.down = f1.down * f2.down;
+    return reduction(result);
+}
+
+Fraction div(Fraction f1, Fraction f2)
+{
+    Fraction result;
+    result.up = f1.up * f2.down;
+    result.down = f1.down * f2.up;
+    return reduction(result);
+}
+
+void showResult(Fraction r)
+{
+    r = reduction(r);
+    if (r.down == 1) printf("%lld", r.up);
+    else if (abs(r.up) > abs(r.down))
+    {
+        printf("%d %d/%d", r.up / r.down, abs(r.up) % r.down, r.down);
+    }
+    else printf("%d/%d", r.up, r.down);
+}
+
+int main()
+{
+    return 0;
+}
+
