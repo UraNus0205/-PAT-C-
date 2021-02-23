b0dd8a9e0c7710c6f44e86155964ce4b0946c934
UraNus0205 <787551776@qq.com>
2021-02-23 16:32:16 +0800

    2021.2.23

---
+++ PAT A1059/PAT A1059.cpp
@@ -0,0 +1,81 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+const int maxn = 10010;
+int prime[maxn], pNum = 0;
+bool p[maxn] = { false };
+
+struct factor {
+    int x, cnt;
+};
+
+factor fac[10];
+
+bool isPrime(int n)
+{
+    if (n <= 1) return false;
+    int sqr = (int)sqrt(1.0 * n);
+    for (int i = 2; i < sqrt(n); i++)
+    {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+
+void find_prime()
+{
+    for (int i = 2; i < maxn; i++)
+    {
+        if (p[i] == false)
+        {
+            prime[pNum++] = i;
+            for (int j = i + i; j < maxn; j += i)
+            {
+                p[j] = true;
+            }
+        }
+    }
+}
+
+int main()
+{
+    find_prime();
+    int n, num = 0;
+    cin >> n;
+    if (n == 1) cout << "1=1";
+    else
+    {
+        cout << n << '=';
+        int sqr = (int)sqrt(1.0 * n);
+        for (int i = 0; i < pNum && prime[i] <= sqr; i++)
+        {
+            if (n % prime[i] == 0)
+            {
+                fac[num].x = prime[i];
+                fac[num].cnt = 0;
+                while (n % prime[i] == 0)
+                {
+                    fac[num].cnt++;
+                    n /= prime[i];
+                }
+                num++;
+            }
+            if (n == 1) break;
+        }
+        if (n != 1)
+        {
+            fac[num].x = n;
+            fac[num++].cnt = 1;
+        }
+        for (int i = 0; i < num; i++)
+        {
+            if (i > 0)cout << "*";
+            cout << fac[i].x;
+            if (fac[i].cnt > 1)
+                cout << "^" << fac[i].cnt;
+        }
+    }
+    return 0;
+}
+
+
---
+++ PAT B1013/PAT B1013.cpp
@@ -0,0 +1,52 @@
+﻿#include <cmath>
+#include <iostream>
+using namespace std;
+const int maxn = 100100;
+int prime[maxn], pNum = 0;
+bool p[maxn] = { false };
+bool isPrime(int n)
+{
+    if (n <= 1) return false;
+    int sqr = (int)sqrt(1.0 * n);
+    for (int i = 2; i <= sqr; i++)
+    {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+
+void find_prime()
+{
+    for (int i = 2; i < maxn; i++)
+    {
+        if (p[i] == false)
+        {
+            prime[pNum++] = i;
+            for (int j = i + i; j < maxn; j = j + i)
+                p[j] = true;
+        }
+    }
+}
+
+int main()
+{
+    find_prime();
+    int m, n;
+    while (cin >> m >> n)
+    {
+        int time = 0;
+        for (int i = m - 1; i < n; i++)
+        {
+            time++;
+            if (time % 10 != 0 && time != (n - m + 1))
+            {
+                cout << prime[i] << " ";
+            }
+            else
+            {
+                cout << prime[i] << endl;
+            }
+        }
+    }
+    return 0;
+}
