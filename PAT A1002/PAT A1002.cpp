72542a452290c6e8274bee65d12bbd0bcc5e91b3
UraNus0205 <787551776@qq.com>
2021-03-03 23:54:53 +0800

    2021.3.3

---
+++ PAT A1002/PAT A1002.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+using namespace std;
+
+const int maxn = 1010;
+double mp[maxn] = { 0 };
+int main()
+{
+    int time = 2;
+    int n, sum = 0;
+    while(time--)
+    {
+        cin >> n;
+        for (int i = 0; i < n; i++)
+        {
+            int key;
+            double value;
+            cin >> key >> value;
+            if (mp[key] == 0) sum++;
+            mp[key] += value;
+            if (mp[key] == 0) sum--;
+        }
+    }
+    cout << sum;
+    for (int i = 1000; i >= 0; i--)
+    {
+        if (mp[i] != 0)
+        {
+            printf(" %d %.1lf", i, mp[i]);
+        }
+    }
+    return 0;
+}
---
+++ PAT A1009/PAT A1009.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+using namespace std;
+double a[1010] = { 0 };
+double b[2010] = { 0 };
+int main()
+{
+    int n, m;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int key;
+        double value;
+        cin >> key >> value;
+        a[key] += value;
+    }
+    cin >> m;
+    for (int i = 0; i < m; i++)
+    {
+        int key;
+        double value;
+        cin >> key >> value;
+        for (int j = 0; j <=1000; j++)
+        {
+            b[key + j] += value * a[j];
+        }
+    }
+    
+    int sum = 0;
+    for (int i = 0; i <= 2000; i++)
+    {
+        if (b[i] != 0) sum++;
+    }
+    cout << sum;
+    for (int i = 2000; i >= 0; i--)
+    {
+        if (b[i] != 0)
+            printf(" %d %.1lf", i, b[i]);
+    }
+    cout << endl;
+    return 0;
+}
