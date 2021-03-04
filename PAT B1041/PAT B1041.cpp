93604fabdd8e98a798a063efb3c0c55fad43de61
UraNus0205 <787551776@qq.com>
2021-03-04 18:59:01 +0800

    2021.3.4

---
+++ PAT B1041/PAT B1041.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+struct stu {
+	string num;
+	int test;
+}s[1010];
+
+int main()
+{
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		string num;
+		int pre_, test_;
+		cin >> num >> pre_ >> test_;
+		s[pre_].num = num;
+		s[pre_].test = test_;
+	}
+	int m;
+	cin >> m;
+	for (int i = 0; i < m; i++)
+	{
+		int a;
+		cin >> a;
+		cout << s[a].num << " " << s[a].test << endl;
+	}
+	return 0;
+}
+
